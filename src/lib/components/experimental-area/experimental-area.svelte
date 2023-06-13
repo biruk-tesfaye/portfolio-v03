d39987eb1bce584cc1201dbabc847e7c12a34e77
Biruk Tesfaye <biruktesfayeve@gmail.com>
2023-06-13 12:36:45 +0300

    feat: add horizontal scroll improvement

--- src/lib/components/experimental-area/experimental-area.svelte
+++ src/lib/components/experimental-area/experimental-area.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	let w: number;
+	let containerSnap: HTMLDivElement;
+
+	const handleScroll = (scrollWidth: number) => {
+		containerSnap.scrollLeft += scrollWidth;
+	};
+</script>
+
 <div class="mx-auto max-w-7xl px-8 pt-6">
 	<h2 class="font-semibold text-xl mt-6">Experiments / Playground</h2>
 	<h3 class="text-base font-light">
@@ -5,8 +14,12 @@
 	</h3>
 
 	<div class="pt-8 md:pt-16 md:px-8">
-		<div class="pb-8 flex snap-x overflow-x-scroll gap-32 container-snap">
+		<div
+			bind:this={containerSnap}
+			class="pb-8 flex snap-x overflow-x-scroll gap-8 md:gap-16 lg:gap-32 container-snap"
+		>
 			<iframe
+				bind:clientWidth={w}
 				class="min-w-[900px] snap-start relative text-center"
 				height="452"
 				scrolling="no"
@@ -43,6 +56,11 @@
 			</iframe>
 		</div>
 		<p class="text-sm text-black/80">&#9432; Scroll horizontally</p>
+
+		<div class="flex items-center justify-center gap-4 pt-4">
+			<button on:click={() => handleScroll(-w)} class="w-4 h-4 bg-[#CDF4EC] focus:bg-[#41FF8D]" />
+			<button on:click={() => handleScroll(w)} class="w-4 h-4 bg-[#CDF4EC] focus:bg-[#41FF8D]" />
+		</div>
 	</div>
 </div>
 
@@ -56,5 +74,6 @@
 	.container-snap {
 		-ms-overflow-style: none; /* IE and Edge */
 		scrollbar-width: none; /* Firefox */
+		scroll-behavior: smooth;
 	}
 </style>
